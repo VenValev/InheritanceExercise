4d6930d51c2e22435b58da4dc4b9e8fe43e7f1b5
Vencislav <vencislav67@hotmail.com>
2022-07-23 12:04:41 +0300

    Add project files.

---
+++ Person/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person()
+        {
+
+        }
+    }
+}
