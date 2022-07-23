03ced71d1cdc59b44e519c4caa546f6fff119a42
Vencislav <vencislav67@hotmail.com>
2022-07-23 14:21:24 +0300

    3

---
+++ PlayersAndMonsters/Hero.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters
+{
+    public class Hero
+    {
+        private string name;
+        private int level;
+
+        public Hero(string name, int level)
+        {
+            this.Name = name;
+            this.Level = level;
+        }
+
+        public string Name 
+        { 
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+        public int Level
+        {
+            get => this.level;
+            set => this.level = value;
+        }
+
+    }
+}
