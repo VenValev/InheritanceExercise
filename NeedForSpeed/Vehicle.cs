221d27f0bef611ba199e15bd2e7208112be56e01
Vencislav <vencislav67@hotmail.com>
2022-07-23 16:39:11 +0300

    4

--- NeedForSpeed/Vehicle.cs
+++ NeedForSpeed/Vehicle.cs
@@ -6,27 +6,71 @@ namespace NeedForSpeed
 {
     public class Vehicle
     {
+        //Fields
         private double defFuelConsumtion;
         private double fuelConsumption;
         private double fuel;
-        private int horspePower;
+        private int horsePower;
 
-
+        //Constructor
         public Vehicle(int horsePower, double fuel)
         {
-
+            this.HorsePower = horsePower;
+            this.Fuel = fuel;
         }
-
+        //Properties
         public double DefFuelConsumtion 
         { 
             get => this.defFuelConsumtion;
-            set => this.defFuelConsumtion = value;
+            set => this.defFuelConsumtion = 1.25;
+        }
+        
+        public double FuelConsumption
+        {
+            get
+            {
+                return this.fuelConsumption;
+            }
+            set
+            {
+                this.fuelConsumption = value;
+            }
+        }
+        public double Fuel 
+        { 
+            get => this.fuel;
+
+            set
+            {
+                if(value < 0)
+                {
+                    this.fuel = 0;
+                }
+                else
+                {
+                    this.fuel = value;
+                }
+            }
+        }
+        public int HorsePower 
+        {
+            get 
+            { 
+                return this.horsePower; 
+            }
+            set
+            {
+                horsePower = value;
+            }
         }
 
-
+        //Methods
         virtual public void Drive(double kilometers)
         {
-
+            while(Fuel - (kilometers * this.FuelConsumption) > 0)
+            {
+                Fuel -= kilometers * this.FuelConsumption;
+            }
         }
     }
 }
