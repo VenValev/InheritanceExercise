3c8a2a477c9152e2dadad3c33bc558129c0c7e90
Vencislav <vencislav67@hotmail.com>
2022-07-23 16:25:18 +0300

    4

---
+++ NeedForSpeed/Vehicle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        private double defFuelConsumtion;
+        private double fuelConsumption;
+        private double fuel;
+        private int horspePower;
+
+
+        public Vehicle(int horsePower, double fuel)
+        {
+
+        }
+
+        virtual public void Drive(double kilometers)
+        {
+
+        }
+    }
+}
