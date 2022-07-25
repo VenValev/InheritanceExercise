ca398dfe8905bced73860ab0f2781b0322c91095
Vencislav <vencislav67@hotmail.com>
2022-07-25 12:19:39 +0300

    5

---
+++ Restaurant/Coffee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Coffee : HotBeverages
+    {
+        public const double coffeeMilliliters = 50;
+        public const decimal coffeePrice = 3.50m;
+
+        private double caffeine;
+        public Coffee(string name,double caffeine)
+            : base(name, coffeePrice, coffeeMilliliters)
+        {
+            this.Caffeine = caffeine;
+        }
+
+        public double Caffeine
+        {
+            get { return this.caffeine; }
+            set { this.caffeine = value; }
+        }
+    }
+}
