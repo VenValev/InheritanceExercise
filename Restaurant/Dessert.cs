2a891d840230a3ad54724b46799a1f6e9119ec01
Vencislav <vencislav67@hotmail.com>
2022-07-25 12:25:23 +0300

    5

---
+++ Restaurant/Dessert.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Dessert : Food
+    {
+        public Dessert(string name, decimal price, double grams) 
+            : base(name, price, grams)
+        {
+        }
+    }
+}
