b31b4b2ebbb6a180014936c0df2ad6d2d04bd4d7
Vencislav <vencislav67@hotmail.com>
2022-07-25 16:01:58 +0300

    6

--- Animals/StartUp.cs
+++ Animals/StartUp.cs
@@ -1,10 +1,61 @@
-﻿namespace Animals
+﻿using System;
+using System.Collections.Generic;
+
+namespace Animals
 {
     public class StartUp
     {
         public static void Main(string[] args)
         {
-            
+            List<Animal> animals = new List<Animal>();
+
+            string command;
+
+            while ((command = Console.ReadLine()) != "Beast!")
+            {
+                string[] animalInfo = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string name = animalInfo[0];
+                int age = int.Parse(animalInfo[1]);
+                
+
+                Animal animal = null;
+
+                if(command == "Dog")
+                {
+                    string gender = animalInfo[2];
+                    animal = new Dog(name, age, gender);
+                }
+                else if(command == "Frog")
+                {
+                    string gender = animalInfo[2];
+                    animal = new Frog(name, age, gender);
+                }
+                else if(command == "Cat")
+                {
+                    string gender = animalInfo[2];
+                    animal = new Cat(name, age, gender);
+                }
+                else if(command == "Kitten")
+                {
+                    animal = new Kitten(name, age);
+                }
+                else if(command == "Tomcat")
+                {
+                    animal = new TomCat(name, age);
+
+                }
+                else
+                {
+                    throw new InvalidOperationException("Invalid input!");
+                }
+                animals.Add(animal);
+            }
+
+            foreach(var animal in animals)
+            {
+                Console.WriteLine(animal);
+            }
         }
     }
 }
