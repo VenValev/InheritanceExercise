746e16dfc92a75c0ffc152f878300265a6aa21dd
Vencislav <vencislav67@hotmail.com>
2022-07-25 12:36:05 +0300

    5

--- Restaurant/ColdBeverage.cs
+++ Restaurant/ColdBeverage.cs
@@ -6,7 +6,8 @@ namespace Restaurant
 {
     public class ColdBeverage : Beverage
     {
-        public ColdBeverage(string name, decimal price, double milliliters) : base(name, price, milliliters)
+        public ColdBeverage(string name, decimal price, double milliliters) 
+            : base(name, price, milliliters)
         {
         }
     }
