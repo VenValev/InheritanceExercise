0467bf7af679ed44ddaa9d614f7e3b467c7504cf
Vencislav <vencislav67@hotmail.com>
2022-07-25 13:03:09 +0300

    6

---
+++ Animals/Animals.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public abstract class Animals
+    {
+        private string name;
+        private int age;
+        private string gender;
+        protected Animals(string name, int age, string gender)
+        { 
+            this.Name = name;
+            this.Age = age;
+            this.Gender = gender;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+    }
+}
